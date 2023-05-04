6e30f4f8d4192970b0e433d57117d9ddd16c3f93
ludals <ludals2446@gmail.com>
2023-05-04 23:06:35 +0900

    file: edit start bat file

--- lavalink.bat
+++ lavalink.bat
@@ -1,3 +1,5 @@
 @echo off
 chcp 65001
+title Lavalink
 "C:\jdk\jdk-17.0.3.1\bin\java.exe" -jar Lavalink_3.4.0_1350.jar
+pause
---
+++ start_without_lavalink.bat
@@ -0,0 +1,7 @@
+
+chcp 65001
+title E-Mp3
+
+
+.venv\Scripts\python.exe -u main.py --lavalink:false
+pause

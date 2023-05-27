17a5c4ba82783228ae60180834717e9ac35f4d7d
ludals <ludals2446@gmail.com>
2023-05-27 16:35:48 +0900

    feat: utilbox - ludals

--- libs/utilbox.py
+++ libs/utilbox.py
@@ -1,5 +1,6 @@
 from discord import Interaction
 import config
+import uuid
 
 def isAdmin(interaction: Interaction, returnMsg: str = '❌ 이 명령어는 관리자만 사용이 가능해요.'):
     """
@@ -10,3 +11,11 @@ def isAdmin(interaction: Interaction, returnMsg: str = '❌ 이 명령어는 관
         return f'`{returnMsg}`'
     else:
         return True
+
+def new_uuid() -> str:
+    """- 새 UUID 생성"""
+    return str(uuid.uuid4())
+
+def check_heart(user_id: int) -> bool:
+    """- 유저가 한디리 하트를 눌렀나 확인"""
+    pass
